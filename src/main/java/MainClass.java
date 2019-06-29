5fb0b3420a11a28b8af590e66808fdcf2a01f129
koskovaleff <kos.kovaleff@gmail.com>
2019-06-30 00:25:11 +0300

    added update on SignUpPage and MainClass

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
+
+    static WebDriver driver;
+
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "e:\\JavaProjects\\Selenide\\onlySeleniumTest\\drivers\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://github.com/");
+
+        MainPage mainPage = new MainPage(driver);
+
+        mainPage.register("testuser", "test@qa.ua", "longtest");
+
 
     }
+
+
+
 }
--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignUpPage {
@@ -6,5 +7,26 @@ public class SignUpPage {
 
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    private By heading = By.xpath("/html/body/div[4]/main/div/div/div[1]/h1");
+    private By createAccountButton = By.xpath("//*[@id=\"signup_button\"]");
+    private By userNameField = By.xpath("//*[@id=\"user_login\"]");
+    private By userEmailField = By.xpath("//*[@id=\"user_email\"]");
+    private By userPasswordField = By.xpath("//*[@id=\"user_password\"]");
+
+    public SignUpPage typeUserName(String username) {
+        driver.findElement(userNameField).sendKeys(username);
+        return this;
+    }
+
+    public SignUpPage typeUserEmail(String email) {
+        driver.findElement(userEmailField).sendKeys(email);
+        return this;
+    }
+
+    public SignUpPage typeUserPassword(String password) {
+        driver.findElement(userPasswordField).sendKeys(password);
+        return this;
     }
 }
