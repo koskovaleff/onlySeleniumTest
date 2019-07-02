2adfdfda7c87e2ad1f65f34288fa81d9e7a3eb36
Kostiantyn Kovalov <kos.kovaleff@gmail.com>
2019-07-02 08:44:12 -0500

    added tests

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -14,6 +14,10 @@ public class SignUpPage {
     private By userNameField = By.xpath("//*[@id=\"user_login\"]");
     private By userEmailField = By.xpath("//*[@id=\"user_email\"]");
     private By userPasswordField = By.xpath("//*[@id=\"user_password\"]");
+    private By mainError = By.xpath(".//*[@id='user_login']/ancestor::dl/preceding-sibling::div[contains(@class, 'flash-error')]");
+    private By userNameError = By.xpath("//input[@id='user_login']/ancestor::dd/following-sibling::dd");
+    private By emailError = By.xpath(".//*[@id='user_email']/ancestor::dd/following-sibling::dd");
+    private By passwordError = By.xpath(".//*[@id='user_password']/ancestor::dd/following-sibling::dd");
 
     public SignUpPage typeUserName(String username) {
         driver.findElement(userNameField).sendKeys(username);
@@ -29,4 +33,33 @@ public class SignUpPage {
         driver.findElement(userPasswordField).sendKeys(password);
         return this;
     }
+
+    public String getHeadingText() {
+        return driver.findElement(heading).getText();
+    }
+
+    public SignUpPage registerWithInvalidCreds(String username, String email, String password){
+        this.typeUserName(username);
+        this.typeUserEmail(email);
+        this.typeUserPassword(password);
+        driver.findElement(createAccountButton).click();
+        return new SignUpPage(driver);
+    }
+
+    public String getMainErrorText(){
+        return driver.findElement(mainError).getText();
+    }
+
+    public String getUsernameErrorText(){
+        return driver.findElement(userNameError).getText();
+    }
+
+    public String getPasswrodErrorText(){
+        return driver.findElement(passwordError).getText();
+    }
+
+    public String getEmailErrorText(){
+        return driver.findElement(emailError).getText();
+    }
+
 }
---
+++ src/test/java/MainPageTest.java
@@ -0,0 +1,60 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainPageTest {
+
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "e:\\JavaProjects\\Selenide\\onlySeleniumTest\\drivers\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://github.com/");
+
+        mainPage = new MainPage(driver);
+    }
+
+    @Test
+    public void signIn() {
+        LoginPage loginPage = mainPage.clickSignInLink();
+        String heading = loginPage.getHeadingText();
+        Assert.assertEquals("Sign in to GitHub", heading);
+    }
+
+    @Test
+    public void registerFailTest() {
+        SignUpPage signUpPage = mainPage.register("testuser", "testemail@yahoo.com", "testpass");
+        String error = signUpPage.getMainErrorText();
+        Assert.assertEquals("There were problems creating your account.", error);
+    }
+
+    @Test
+    public void signUpEmptyUsernameTest(){
+        SignUpPage signUpPage = mainPage.register("", "mail", "pass");
+        String error = signUpPage.getUsernameErrorText();
+        Assert.assertEquals("Login can't be blank", error);
+    }
+
+    @Test
+    public void signUpInvalidEmailTest(){
+        SignUpPage signUpPage = mainPage.register("qeqwe", "qweq", "pass");
+        String error = signUpPage.getEmailErrorText();
+        Assert.assertEquals("Email is invalid or already taken", error);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
